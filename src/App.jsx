a9cb8790b736f3ee58fe0e544c1c34e491b74054
Your Name <innocentchime30@gmail.com>
2024-03-05 12:36:10 +0100

    initia commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import UserList from './components/UserList'
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <UserList />
+      </header>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UserList.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(import.meta.env.VITE_API_URL);
+        setListOfUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-xl text-center uppercase text-cyan-700">User List</h1>
+      <ul className="grid grid-cols-2 items-center justify-center overflow-hidden">
+        {listOfUsers.map((user) => (
+          <li key={user.id}>
+            <strong>ID: </strong>
+            {user.id} <br />
+            <strong> Name: </strong> {user.name} <br />
+            <strong>Username: </strong> {user.username} <br />
+            <strong>Email: </strong>
+            {user.email} <br />
+            <strong>Address:</strong> {user.address.street}{" "}
+            <strong>Suite: </strong>
+            {user.address.suite} <strong>City: </strong> {user.address.city}{" "}
+            <strong>Zipcode</strong> {user.address.zipcode} <br />
+            <strong>Phone Number: </strong> {user.phone} <br />
+            <strong>Website: </strong> {user.website} <br />
+            <strong>Company: </strong> {user.company.name} <br />
+            <strong>Company's Catchphrase: </strong> {user.company.catchPhrase}{" "}
+            <br />
+            <strong>Business: </strong> {user.company.bs}
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
